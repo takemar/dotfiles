2b9c94a0a832b719cefe7d22b41496dfff7edfda
takemaro <info@takemaro.com>
2026-04-27 02:45:02 +0900

    add firefox user.json

---
+++ resources/firefox/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.compactmode.show", true);
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.uidensity", 1);
+user_pref("pdfjs.cursorToolOnLoad", 1);
+user_pref("pdfjs.defaultZoomValue", "page-actual");
